7d927506436f1de751888ed8d9f57ba9b6ae6660
Elber Gutierrez <elgu404@gmail.com>
2024-10-02 00:24:42 -0400

    Worked on more updates for GUI

--- MazeGUI.java
+++ MazeGUI.java
@@ -8,39 +8,64 @@ import java.util.Stack;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
+import javax.swing.filechooser.FileSystemView;
 
 public class MazeGUI extends JFrame{
 
+	titleMazePanel titleMaze;
+	
 	JPanel mazePanel;
+	JPanel titlePanel;
+	
+	
+	// Used for File Chooser
+	JFileChooser importFile = new JFileChooser("C\\",FileSystemView.getFileSystemView());
 	
 
 	public MazeGUI() {
 		JFrame mainFrame = new JFrame("Maze Runner");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(700,700);
+		setSize(510, 530);
 		setLayout(new BorderLayout());
+		setBackground(Color.BLACK);
 
-		// Title 
 		
+		// subPanel for title panel
+		titlePanel = new JPanel();
+		titlePanel.setBackground(Color.BLACK);
 		
+		// JLabels
+		JLabel titleName = new JLabel("Maze Runner");
+		titleName.setForeground(Color.WHITE);
+		titleName.setFont(new Font("Times New Roman", Font.PLAIN, 75));
 		
 		// Sub-panel for maze panel
 		mazePanel = new JPanel();
-		mazePanel.setBackground(Color.gray); // For contrast
+		mazePanel.setBackground(Color.BLACK); // For contrast
 
 		// Sub-panel for button panel
 		JPanel buttonPanel = new JPanel();
+		buttonPanel.setBackground(Color.BLACK);
 
-		// JButton solve
+		// JButton 
 		JButton loadButton = new JButton("Load Maze");
 		JButton solveButton = new JButton("Solve Maze");
 		JButton resetButton = new JButton("Reset Maze");
-
+		
+		loadButton.setBackground(Color.BLACK);
+		loadButton.setForeground(Color.WHITE);
+		solveButton.setBackground(Color.BLACK);
+		solveButton.setForeground(Color.WHITE);
+		resetButton.setBackground(Color.BLACK);
+		resetButton.setForeground(Color.WHITE);
+		
 		// Actions for Buttons
-		loadButton.addActionListener(e -> displayMaze());  // TODO Change to find a file through file explorer
+		loadButton.addActionListener(e -> importMaze());  // TODO Change to find a file through file explorer
 		solveButton.addActionListener(e -> visulizeSolution(Maze.path));
 		resetButton.addActionListener(e -> displayMaze());
 
@@ -49,24 +74,23 @@ public class MazeGUI extends JFrame{
 		buttonPanel.add(solveButton);
 		buttonPanel.add(resetButton);
 
+		//Add Components to title panel
+		titlePanel.add(titleName);
+		
+		// Create and store reference to titleMazePanel
+		titleMaze = new titleMazePanel();
+		
 		// Add sub-panel to mainFrame
-	//	TODO add(title(), BorderLayout.NORTH);
-		add(mazePanel, BorderLayout.CENTER);
+		add(titlePanel, BorderLayout.NORTH);
+		//add(mazePanel, BorderLayout.CENTER);
+		add(titleMaze, BorderLayout.CENTER);
 		add(buttonPanel, BorderLayout.SOUTH);		
 	}
 	
-	// TODO
-	/*private void titlePanel extends  JPanel() {
-		Graphics2D g2 = (Graphics2D)g;
-		g2.setFont(g2.getFont().deriveFont(Font.BOLD,96F));
-		String text = "Maze Runner";
-		g2.setColor(Color.WHITE);
-		g2.drawString(text, 50, 70);
-	} */
-	
-
-	
 	private void displayMaze() {
+		
+		remove(titleMaze);   // Remove the animated panel
+	    add(mazePanel, BorderLayout.CENTER);
 		mazePanel.removeAll();
 		mazePanel.setLayout(new GridLayout(10, 10));
 		Border cellBorder = BorderFactory.createLineBorder(Color.DARK_GRAY); // Define cell borders
@@ -97,4 +121,13 @@ public class MazeGUI extends JFrame{
 			cell.setBackground(Color.PINK);
 		}
 	}
+
+	private void importMaze() {
+		importFile.showSaveDialog(null);
+		
+		if (importFile.getSelectedFile() != null) {
+			importFile.getSelectedFile().getAbsolutePath();
+		}
+	}
+	
 }
